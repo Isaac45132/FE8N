2d0ae265d7cd57446d7b83dd11f4e10bec96c601
Isaac45132 <47072705+Isaac45132@users.noreply.github.com>
2020-07-25 12:47:41 +0900

    チクチク防止
    チクチク対策のまとめ
    計算式：未撃破経験値－（チクチク値－３　＊最低０）
    ①チクチク値が10未満なら最低経験値を1にする（ミスやノーダメも1）
    ②チクチク値が10以上なら最低経験値を0にする（ミスやノーダメも0）
    ③ハードモードのみ適応される
    ④虚無の呪いもちゃんと発動する
    ⑤撃破経験値は一切減らない

--- Engine Hacks/_N/tikutiku/tikutikukeikenti.s
+++ Engine Hacks/_N/tikutiku/tikutikukeikenti.s
@@ -26,14 +26,33 @@ and	r1, r2
 cmp	r1, #0
 beq	end		@ハードモードでないなら終了
 mov	r1, #0x37
-add	r1, r1, r7
-ldrb	r1, [r1, #0]
+add	r3, r1, r7
+ldrb	r1, [r3, #0]
 sub	r1, #4		@経験値減算を開始するチクチク値
-bmi	end		@チクチク値が4未満なら終了
+bmi	saiteiti	@チクチク値が4未満なら終了
 add	r1, #1
 sub	r0, r0, r1	@獲得経験値
-bpl	end
-mov	r0, #0		@負の値にならないようにする
+
+saiteiti:
+cmp	r0, #1
+bpl	end		@獲得経験値が1以上なら終了
+mov	r0, #1		@最低経験値1
+ldr	r1, [r4, #0]	@ユニット判定
+ldr	r2, [r4, #4]	@クラス判定
+ldr	r1, [r1, #0x28]	@ユニット特性
+ldr	r2, [r2, #0x28]	@クラス特性
+lsr	r1, r1, #24	@特性4
+lsr	r2, r2, #24	@特性4
+orr	r1, r2
+cmp	r1, #1		@虚無の呪い
+beq	zero
+ldrb	r1, [r3, #0]
+cmp	r1, #10
+bmi	end
+
+zero:
+mov	r0, #0		@10戦目以降は最低経験値0
+
 end:
 pop	{r1, r2}
 pop	{pc}
