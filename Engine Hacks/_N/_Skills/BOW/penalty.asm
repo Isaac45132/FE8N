b30f19e3024ad67a941077a90415edcd90d698e9
Isaac45132 <47072705+Isaac45132@users.noreply.github.com>
2020-09-02 21:23:56 +0900

    スキル『魔道剣技』
    剣装備時、自分の力か魔力か高い方でダメージ計算＆射程＋１

--- Engine Hacks/_N/_Skills/BOW/penalty.asm
+++ Engine Hacks/_N/_Skills/BOW/penalty.asm
@@ -1,5 +1,5 @@
-@thumb
-;@org	$0802abd4
+.thumb
+@org	$0802abd4
 	add		r1, #98
 	mov		r3, r1
 	add		r0, #72
@@ -7,7 +7,7 @@
 	bl		range
 	mov		r1, #15
 	and		r0, r1
-	ldr		r1, =$0203a4d0
+	ldr		r1, =0x0203a4d0
 	ldrb	r1, [r1, #2]
 	cmp		r1, r0
 	bgt		penalty
@@ -16,7 +16,7 @@
 	ldrh	r0, [r2, r0]
 	bl		range
 	lsr		r0, r0, #4
-	ldr		r1, =$0203a4d0
+	ldr		r1, =0x0203a4d0
 	ldrb	r1, [r1, #2]
 	cmp		r1, r0
 	blt		penalty
@@ -25,17 +25,30 @@
 	mov		r1, #96
 	ldrh	r2, [r2, r1]
 	b		end
-penalty
+penalty:
+	ldr	r0, [r2, #4]
+	ldrb	r0, [r0, #4]
+	cmp	r0, #0x29	@射程増加による命中マイナスを除外するクラス
+	beq	musi
+	cmp	r0, #0x2A	@射程増加による命中マイナスを除外するクラス
+	bne	mainasu
+musi:
 	mov		r0, r2
 	mov		r1, #96
 	ldrh	r2, [r2, r1]
-	sub		r2 #30
-end
+	b		end
+
+mainasu:
+	mov		r0, r2
+	mov		r1, #96
+	ldrh	r2, [r2, r1]
+	sub		r2, #30
+end:
 	ldrh	r1, [r3]
-	ldr		r3, =$0802abdc
+	ldr		r3, =0x0802abdc
 	mov		pc, r3
 	
-range
-	ldr		r1, =$08017448
+range:
+	ldr		r1, =0x08017448
 	mov		pc, r1
 	
