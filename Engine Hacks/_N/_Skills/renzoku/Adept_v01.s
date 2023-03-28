11c754de0c7c9a278fdde9d99a8a2dddedd51192
Isaac45132 <47072705+Isaac45132@users.noreply.github.com>
2023-03-28 23:16:48 +0900

    突撃時は連続攻撃スキル無効

--- Engine Hacks/_N/_Skills/renzoku/Adept_v01.s
+++ Engine Hacks/_N/_Skills/renzoku/Adept_v01.s
@@ -3,6 +3,11 @@
 @org	0802b004
     push {r4, lr}
     mov r4, #0
+
+	bl HasCharge
+	cmp r2, #1
+	beq endBrave	@突撃発動時は連続攻撃不可
+
     ldr r0, [r0, #76]
     mov r1, #32
     and r0, r1
@@ -37,6 +42,33 @@ Addition:
     mov r2, #0  @獅子連判定用
     ldr r3, ADDRESS
     mov pc, r3
+
+HasCharge:
+        push {r0, r4, lr}
+        mov r4, r0
+        mov r2, #94
+        ldsh r0, [r4, r2]
+        ldsh r2, [r1, r2]
+        cmp r0, r2
+        ble notCharge @速さが足りない！
+        ldrb r0, [r4, #0x13]
+        ldrb r2, [r1, #0x13]
+        cmp r0, r2
+        ble notCharge @HPが足りない！
+    
+        mov r0, r4
+            ldr r2, ADDRESS+4 @突撃
+            mov lr, r2
+            .short 0xF800
+        cmp r0, #0
+        beq notCharge
+        mov r2, #1
+        b endCharge
+    notCharge:
+        mov r2, #0
+    endCharge:
+        pop {r0, r4, pc}
+
 
 .align
 .ltorg
--- Engine Hacks/_N/_Skills/renzoku/yosoku.s
+++ Engine Hacks/_N/_Skills/renzoku/yosoku.s
@@ -3,6 +3,10 @@
 .thumb
     push {r2, r3}
 
+	bl HasCharge
+	cmp r0, #1
+	beq end		@突撃発動時は連続攻撃不可
+
     ldr r0, [r4, #76]
     mov r1, #32
     and r0, r1
@@ -11,6 +15,7 @@
     bl adept
     cmp r0, #1
     beq got
+end:
     ldr r0, =0x0803679a @end
     .short 0xE000
 got:
@@ -28,6 +33,36 @@ adept:
         mov r2, #1  @獅子連判定用
         ldr r3, addr
         mov pc, r3
+
+HasCharge:
+        push {r2, lr}
+	ldr r1, =0x0203a4e8
+	cmp r1, r4
+	bne mikataC
+	ldr r1, =0x0203a568
+mikataC:
+        mov r2, #94
+        ldsh r0, [r4, r2]
+        ldsh r2, [r1, r2]
+        cmp r0, r2
+        ble notCharge @速さが足りない！
+        ldrb r0, [r4, #0x13]
+        ldrb r2, [r1, #0x13]
+        cmp r0, r2
+        ble notCharge @HPが足りない！
+    
+        mov r0, r4
+            ldr r2, addr+4 @突撃
+            mov lr, r2
+            .short 0xF800
+        cmp r0, #0
+        beq notCharge
+        mov r0, #1
+        b endCharge
+    notCharge:
+        mov r0, #0
+    endCharge:
+        pop {r2, pc}
 
 .align
 .ltorg
