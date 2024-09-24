33d5de309e67dbf57767d7b2ecabaec4c4880709
Isaac45132 <47072705+Isaac45132@users.noreply.github.com>
2024-09-24 21:23:42 +0900

    さまなーバグ

--- Engine Hacks/_N/tikutiku/summon_tiku.s
+++ Engine Hacks/_N/tikutiku/summon_tiku.s
@@ -2,15 +2,17 @@
 
 @org	0x080324b0
 push	{r0, r1, r2}
-mov	r1, #0x43	@AI1カウンター	
-ldrb	r0, [r2, r1]
+ldr	r1, =0x03004DF0
+ldr	r1, [r1]
+add	r1, #0x43	@AI1カウンター	
+ldrb	r0, [r1]
 ldr	r2, adr		@疲労値
 add	r0, r0, r2
 cmp	r0, #99		@チクチク値限界:99
 bls	dainyuu
 mov	r0, #99
 dainyuu:
-strb	r0, [r1, #0]	@チクチク値代入
+strb	r0, [r1]	@チクチク値代入
 pop	{r0, r1, r2}
 
 bl	Exp10
